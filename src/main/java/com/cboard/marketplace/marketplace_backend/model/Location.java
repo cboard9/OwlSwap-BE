155d9a161b323b7df6188a4c69b774ebb994baf1
ryan <ryantet@comcast.net>
2025-04-30 11:45:44 -0400

    linked database locations with new listing drop down, product card map hypothetically working cant test yet

--- src/main/java/com/cboard/marketplace/marketplace_backend/model/Location.java
+++ src/main/java/com/cboard/marketplace/marketplace_backend/model/Location.java
@@ -15,12 +15,19 @@ public class Location
 
     private String name;
 
+    private String address;
+    private Double latitude;
+    private Double longitude;
+
     public Location() {
     }
 
-    public Location(int locationId, String name) {
+    public Location(int locationId, String name, String address, Double latitude, Double longitude) {
         this.locationId = locationId;
         this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public int getLocationId() {
@@ -37,5 +44,29 @@ public class Location
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 }
